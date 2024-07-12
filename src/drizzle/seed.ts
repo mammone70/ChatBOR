ea79a979c9bc9ee615c101a6de8c5a24667b103c
Mammone <mammone@gmail.com>
2024-07-12 13:24:50 -0700

    Strip nonsense out of text before embedding.

--- src/drizzle/seed.ts
+++ src/drizzle/seed.ts
@@ -20,6 +20,11 @@ if (!process.env.DATABASE_URL) {
   throw new Error('process.env.DATABASE_URL is not defined. Please set it.')
 }
 
+const embedder = new OllamaEmbeddings({
+  model: 'nomic-embed-text:latest',
+  baseUrl: "http://localhost:11434",
+});
+
 async function main() {
   try {
     const transcript = await db.query.transcripts.findFirst({
@@ -35,19 +40,28 @@ async function main() {
     throw error
   }
 
-  const dir = "/home/mammone/Downloads/BOR-Transcripts/test";
+  const dir = "/home/mammone/Downloads/BOR-Transcripts/Hamson/test";
 
   console.log("Seeding DB with transcripts from " + dir);
   
+  const transcriptObjects : any[] = [];
+
   const directoryLoader = new DirectoryLoader(
     dir,
     {
-      ".pdf": (path: string) => new PDFLoader(path),
+      ".pdf": 
+        (filePath: string) => {
+          transcriptObjects.push({
+            name: path.basename(filePath),    
+          });
+          return new PDFLoader(filePath)
+        },
     }
   );
   
   const docs = await directoryLoader.load();  
-        
+  
+  console.log(transcriptObjects)
   //split pdf
   const splitter = new RecursiveCharacterTextSplitter({
       chunkSize: 200,
@@ -55,34 +69,35 @@ async function main() {
   });
 
   const splitDocs = await splitter.splitDocuments(docs);
-  console.log(splitDocs[0]);
-  console.log(splitDocs[0].metadata);
 
-  
-  
-  // console.log(splitDocs.length + " docs.");
+  for (const docChunk of splitDocs) {
 
-  // const embedding = await generateEmbedding(file);
-  // await new Promise((r) => setTimeout(r, 500)); // Wait 500ms between requests;
-  // const { embedding, ...p } = record
+    // const embedding = await generateEmbedding(docChunk.pageContent);
+    // console.log(embedding);
+    
+    break;
 
-  // Create the pokemon in the database
-  // const [pokemon] = await db.insert(pokemons).values(p).returning()
+    // await new Promise((r) => setTimeout(r, 500)); // Wait 500ms between requests;
+    // const { embedding, ...p } = record
 
-  // await db
-  //   .update(pokemons)
-  //   .set({
-  //     embedding,
-  //   })
-  //   .where(eq(pokemons.id, pokemon.id))
+    // Create the pokemon in the database
+    // const [pokemon] = await db.insert(pokemons).values(p).returning()
 
-  // console.log(`Added ${pokemon.number} ${pokemon.name}`)
+    // await db
+    //   .update(pokemons)
+    //   .set({
+    //     embedding,
+    //   })
+    //   .where(eq(pokemons.id, pokemon.id))
 
-  // Uncomment the following lines if you want to generate the JSON file
-  // fs.writeFileSync(
-  //   path.join(__dirname, "./pokemon-with-embeddings.json"),
-  //   JSON.stringify({ data }, null, 2),
-  // );
+    // console.log(`Added ${pokemon.number} ${pokemon.name}`)
+
+    // Uncomment the following lines if you want to generate the JSON file
+    // fs.writeFileSync(
+    //   path.join(__dirname, "./pokemon-with-embeddings.json"),
+    //   JSON.stringify({ data }, null, 2),
+    // );
+  }
   console.log('DB Seeded Successfully!')
 }
 
@@ -96,11 +111,20 @@ main()
     process.exit(1)
   })
 
-// async function generateEmbedding(_input: string) {
-//   const input = _input.replace(/\n/g, ' ')
-//   const { embedding } = await embed({
-//     model: openai.embedding('text-embedding-ada-002'),
-//     value: input,
-//   })
-//   return embedding
-// }
+async function generateEmbedding(_input: string) {
+  const formattedInput = formatTextForEmbedding(_input);
+  const embedding = await embedder.embedQuery(formattedInput);
+  return embedding;
+}
+
+/***
+ * Takes input string, strips middle dot ("Georgian Comma")
+ * Removes newlines and replaces with spaces.
+ * Trims each line before joining them all together
+ */
+function formatTextForEmbedding(_input: string) {
+  return  _input.replaceAll('\u00B7', '')
+          .split('\n')
+          .map(element =>  element.trim())
+          .join(' ');
+}
