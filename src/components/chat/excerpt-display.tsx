39d9f07c5978bd8c21adbb6010d5633f31f9dc3f
Mammone <mammone@gmail.com>
2024-07-16 21:50:22 -0700

    Add header to ExcerptDisplay component

--- src/components/chat/excerpt-display.tsx
+++ src/components/chat/excerpt-display.tsx
@@ -9,13 +9,16 @@ export function ExcerptDisplay(props : ExcerptDisplayProps) {
     return (
         <div className="flex h-full flex-col">
           <div className="flex items-center p-2">              
+            <h1 className="text-xl font-bold">
+              Selected Transcript Excerpt
+            </h1>
           </div>
           <Separator />
           {props?.excerpt ? (
             <div className="flex flex-1 flex-col">                
               <Separator />
               <div className="flex-1 whitespace-pre-wrap p-4 text-sm">
-                {props.excerpt  .content}
+                {props.excerpt.content}
               </div>
               <Separator className="mt-auto" />
               <div className="p-4">
