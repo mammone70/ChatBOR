28022ef2e2eae568bdc34f45802cdf6faf568613
Mammone <mammone@gmail.com>
2024-07-28 13:51:51 -0700

    Initial implementation of chat history in RAG pipeline using postgres db for storage

--- src/actions/chat.ts
+++ src/actions/chat.ts
@@ -4,23 +4,46 @@ import { semanticSearchTranscripts } from "@/data/transcripts";
 import { ChatSchema } from "@/schemas";
 import * as z from "zod";
 
-import { PromptTemplate } from "@langchain/core/prompts";
+import { 
+    PromptTemplate, 
+    ChatPromptTemplate,
+    MessagesPlaceholder
+} from "@langchain/core/prompts";
+
 import { ChatOpenAI } from "@langchain/openai";
 import { createStreamableValue } from 'ai/rsc';
 import { getVectorDB } from "@/lib/langchain";
 import { MultiQueryRetriever } from "langchain/retrievers/multi_query";
 import { LLMChainExtractor } from "langchain/retrievers/document_compressors/chain_extract";
 import { ContextualCompressionRetriever } from "langchain/retrievers/contextual_compression";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import { PostgresChatMessageHistory } from "@langchain/community/stores/message/postgres";
+import { pool } from "@/drizzle/db";
 
-const promptTemplate = PromptTemplate.fromTemplate(`Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+// const promptTemplate = PromptTemplate.fromTemplate(`Use the following pieces of context to answer the question at the end.
+// If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-{context}
+// {context}
 
-Question: {question}
+// Question: {question}
 
-Helpful Answer:`
-);
+// Helpful Answer:`
+// );
+
+const qaSystemPrompt = `You are an assistant for question-answering tasks.
+Use the following pieces of retrieved context to answer the question.
+Provide as much detail as possible without getting off topic.
+If you don't know the answer, just say that you don't know.
+
+{context}`
+    
+const contextualizeQSystemPrompt = "Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."
 
 export const chat = async (values: z.infer<typeof ChatSchema>) => {
     const validateFields = ChatSchema.safeParse(values);
@@ -36,7 +59,8 @@ export const chat = async (values: z.infer<typeof ChatSchema>) => {
     (async () => {
         const llm = new ChatOpenAI({
             temperature: 0.9,
-            model: "gpt-3.5-turbo-1106",
+            // model: "gpt-3.5-turbo-1106",
+            model: "gpt-4o-mini",
             // In Node.js defaults to process.env.ANTHROPIC_API_KEY,
             // apiKey: "YOUR-API-KEY",
             maxTokens: 2000,
@@ -45,11 +69,17 @@ export const chat = async (values: z.infer<typeof ChatSchema>) => {
         //create multi query retriever so we can generate multiple
         //queries to improve upon the user's query
         const pgVectorStore = getVectorDB();
-        const multiQueryRetriever = MultiQueryRetriever.fromLLM({
-            llm:llm,
-            retriever : pgVectorStore.asRetriever(),
-            verbose: true,
-        });
+        // const multiQueryRetriever = MultiQueryRetriever.fromLLM({
+        //     llm:llm,
+        //     retriever : pgVectorStore.asRetriever(),
+        //     verbose: true,
+        // });
+
+        const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
+            ["system", contextualizeQSystemPrompt],
+            new MessagesPlaceholder("chat_history"),
+            ["human", "{input}"],
+        ]);
 
         //Create Contextual Compression object so that
         //we can iterate over returned documents to pull
@@ -57,35 +87,75 @@ export const chat = async (values: z.infer<typeof ChatSchema>) => {
         const baseCompressor = LLMChainExtractor.fromLLM(llm);
         const compressionRetriever = new ContextualCompressionRetriever({
             baseCompressor,
-            baseRetriever: multiQueryRetriever,
+            baseRetriever: pgVectorStore.asRetriever(),
         });
 
+        const historyAwareRetriever = await createHistoryAwareRetriever({
+            llm,
+            // retriever : pgVectorStore.asRetriever(),
+            retriever : compressionRetriever,
+            rephrasePrompt : contextualizeQPrompt,
+        });
+
+        const qaPrompt = ChatPromptTemplate.fromMessages([
+            ["system", qaSystemPrompt],
+            new MessagesPlaceholder("chat_history"),
+            ["human", "{input}"],    
+        ]);
+
+        const qaChain = await createStuffDocumentsChain({
+            llm,
+            prompt : qaPrompt,
+        });
+
+        const ragChain = await createRetrievalChain({
+            retriever : historyAwareRetriever,
+            combineDocsChain : qaChain,
+        });
+
+        const chainWithHistory = new RunnableWithMessageHistory({
+            runnable: ragChain,
+            inputMessagesKey : "input",
+            historyMessagesKey : "chat_history",
+            outputMessagesKey : "answer",
+            getMessageHistory: async (sessionId) => {
+                const chatHistory = new PostgresChatMessageHistory({
+                  sessionId,
+                  pool,
+                });
+                return chatHistory;
+            },
+        });
+
+        
         // const transcriptChunks = await semanticSearchTranscripts(message);
 
         //get relevant docs
-        const transcriptChunks = await compressionRetriever.invoke(message);
+        // const transcriptChunks = await compressionRetriever.invoke(message);
 
-        let chunkContext = ""; 
-        transcriptChunks.map((chunk) => {
-            chunkContext += `${chunk.pageContent} `
-        });
+        // let chunkContext = ""; 
+        // transcriptChunks.map((chunk) => {
+        //     chunkContext += `${chunk.pageContent} `
+        // });
 
-        const formattedPrompt = await promptTemplate.format({
-            question : message,
-            context : chunkContext,
-        });
+        // const formattedPrompt = await promptTemplate.format({
+        //     question : message,
+        //     context : chunkContext,
+        // });
 
+        
         // console.log(formattedPrompt);
 
-        const chain = promptTemplate.pipe(llm);
-        const stream = await chain.stream({
-            question : message,
-            context : chunkContext,    
-        });
+        // const chain = promptTemplate.pipe(llm);
+
+        const stream = await chainWithHistory.stream(
+            {input : message},
+            { configurable: { sessionId: "langchain-test-session" } },
+        );
 
         for await (const chunk of stream) {
             // console.log("Chunk: " + chunk.content.toString())
-            streamableStatus.update(chunk.content.toString())
+            streamableStatus.update(chunk.answer);
         }
 
         streamableStatus.done();
