ffbf18ff5634019bbccb3f1da22fe6ebb5056089
Mammone <mammone@gmail.com>
2024-08-12 21:28:06 -0700

    Initial file list component

--- src/components/files/file-list.tsx
+++ src/components/files/file-list.tsx
@@ -1,4 +1,5 @@
 import { Transcript } from "@/data/transcripts"
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "../ui/table"
 
 interface FileListProps {
   files : Transcript[]
@@ -6,7 +7,28 @@ interface FileListProps {
 
 function FileList(props : FileListProps) {
   return (
-    <div>FileList</div>
+    <Table>
+      <TableCaption>All of your transcripts</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Name</TableHead>
+          <TableHead className="text-right">Pages</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {
+          props.files.map(
+            (file) => (
+              <TableRow key={file.id}>
+                <TableCell className="font-medium truncate">{file.name}</TableCell>
+                <TableCell className="text-right">{file.totalPages}</TableCell>
+              </TableRow>
+            )
+          )
+        }
+      </TableBody>
+    </Table>
+
   )
 }
 
